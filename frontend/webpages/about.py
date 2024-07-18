d0e759e06feee07c43f6eb7cfe882a8c556ca9a7
Cipher Shadow <mprogrammierer@gmail.com>
2024-07-18 22:07:43 +0200

    about section names

--- frontend/webpages/about.py
+++ frontend/webpages/about.py
@@ -3,12 +3,10 @@ import streamlit as st
 st.title("What is CineBrowse?")
 st.write("CineBrowse is a movie quiz, where you can test your knowledge on movies and their details.")
 st.subheader("Our Team:")
-st.markdown(
-    """
-    <p align="center">
-    <a href="https://github.com/cipher-shad0w" target="_blank">Cipher Shadow</a> &#xa0; | &#xa0;
-    <a href="https://github.com/arvedb" target="_blank">Arved Bahde</a> &#xa0; | &#xa0;
-    <a href="https://github.com/mirixy" target="_blank">Miriam</a>
-    </p>            
-    """
-    )
+st.markdown("""
+<p align="center">
+<a href="https://github.com/cipher-shad0w" target="_blank">Cipher Shadow</a> &#xa0; | &#xa0;
+<a href="https://github.com/arvedb" target="_blank">Arved Bahde</a> &#xa0; | &#xa0;
+<a href="https://github.com/mirixy" target="_blank">Miriam</a>
+</p>
+""", unsafe_allow_html=True)
